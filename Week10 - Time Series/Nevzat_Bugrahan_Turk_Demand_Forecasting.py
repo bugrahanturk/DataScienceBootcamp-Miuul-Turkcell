46185540e0ee5a3145c3c713a686cf8ca1edc6e8
Nevzat Buğrahan Türk <33989126+bugrahanturk@users.noreply.github.com>
2022-03-30 02:37:32 +0300

    Add files via upload

---
+++ Week10 - Time Series/Nevzat_Bugrahan_Turk_Demand_Forecasting.py
@@ -0,0 +1,272 @@
+import time
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import lightgbm as lgb
+import warnings
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+warnings.filterwarnings('ignore')
+
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+# Loading the data
+
+train = pd.read_csv('Week10/datasets/demand_forecasting/train.csv', parse_dates=['date'])
+test = pd.read_csv('Week10/datasets/demand_forecasting/test.csv', parse_dates=['date'])
+
+df = pd.concat([train, test], sort=False)
+
+# EDA
+
+check_df(df)
+
+df.groupby(["store", "item"]).agg({"sales": ["sum", "mean", "median", "std"]})
+
+# FEATURE ENGINEERING
+df.head()
+
+
+def create_date_features(df):
+    df['month'] = df.date.dt.month
+    df['day_of_month'] = df.date.dt.day
+    df['day_of_year'] = df.date.dt.dayofyear
+    df['week_of_year'] = df.date.dt.weekofyear
+    df['day_of_week'] = df.date.dt.dayofweek
+    df['year'] = df.date.dt.year
+    df["is_wknd"] = df.date.dt.weekday // 4
+    df['is_month_start'] = df.date.dt.is_month_start.astype(int)
+    df['is_month_end'] = df.date.dt.is_month_end.astype(int)
+    return df
+
+
+df = create_date_features(df)
+
+df.groupby(["store", "item", "month"]).agg({"sales": ["sum", "mean", "median", "std"]})
+
+
+# Random Noise
+
+def random_noise(dataframe):
+    return np.random.normal(scale=1.6, size=(len(dataframe),))
+
+
+# Lag/Shifted Features
+
+df.sort_values(by=['store', 'item', 'date'], axis=0, inplace=True)
+
+
+def lag_features(dataframe, lags):
+    for lag in lags:
+        dataframe['sales_lag_' + str(lag)] = dataframe.groupby(["store", "item"])['sales'].transform(
+            lambda x: x.shift(lag)) + random_noise(dataframe)
+    return dataframe
+
+
+df = lag_features(df, [91, 98, 105, 112, 119, 126, 182, 364, 546, 728])
+
+check_df(df)
+
+
+# Rolling Mean Features
+
+def roll_mean_features(dataframe, windows):
+    for window in windows:
+        dataframe['sales_roll_mean_' + str(window)] = dataframe.groupby(["store", "item"])['sales']. \
+                                                          transform(
+            lambda x: x.shift(1).rolling(window=window, min_periods=10, win_type="triang").mean()) + random_noise(
+            dataframe)
+    return dataframe
+
+
+df = roll_mean_features(df, [365, 546])
+
+
+# Exponentially Weighted Mean Features
+
+def ewm_features(dataframe, alphas, lags):
+    for alpha in alphas:
+        for lag in lags:
+            dataframe['sales_ewm_alpha_' + str(alpha).replace(".", "") + "_lag_" + str(lag)] = \
+                dataframe.groupby(["store", "item"])['sales'].transform(lambda x: x.shift(lag).ewm(alpha=alpha).mean())
+    return dataframe
+
+
+alphas = [0.95, 0.9, 0.8, 0.7, 0.5]
+lags = [91, 98, 105, 112, 180, 270, 365, 546, 728]
+
+df = ewm_features(df, alphas, lags)
+check_df(df)
+
+# One-Hot Encoding
+
+df = pd.get_dummies(df, columns=['store', 'item', 'day_of_week', 'month'])
+
+check_df(df)
+
+# Converting sales to log(1+sales)
+
+df['sales'] = np.log1p(df["sales"].values)
+
+check_df(df)
+
+
+# Model
+
+# Custom Cost Function
+# SMAPE: Symmetric mean absolute percentage error (adjusted MAPE)
+
+def smape(preds, target):
+    n = len(preds)
+    masked_arr = ~((preds == 0) & (target == 0))
+    preds, target = preds[masked_arr], target[masked_arr]
+    num = np.abs(preds - target)
+    denom = np.abs(preds) + np.abs(target)
+    smape_val = (200 * np.sum(num / denom)) / n
+    return smape_val
+
+
+def lgbm_smape(preds, train_data):
+    labels = train_data.get_label()
+    smape_val = smape(np.expm1(preds), np.expm1(labels))
+    return 'SMAPE', smape_val, False
+
+
+# Time-Based Validation Sets
+
+train
+test
+
+# Test setimizde 2018 yılı bulunuyor ve bizden istenen 2018 yılının 3 ayının (ben son 3 ayı seçtim) tahmini dolayısıyla
+# buna en uygun senaryo olarak 2017 yılını validation seti olarak seçiyoruz
+
+# 2017'nin başına kadar (2016'nın sonuna kadar) train seti.
+train = df.loc[(df["date"] < "2017-01-01"), :]
+
+# 2017'nin son 3'ayı validasyon seti.
+val = df.loc[(df["date"] >= "2017-09-01") & (df["date"] < "2018-01-01"), :]
+
+cols = [col for col in train.columns if col not in ['date', 'id', "sales", "year"]]
+
+Y_train = train['sales']
+X_train = train[cols]
+
+Y_val = val['sales']
+X_val = val[cols]
+
+Y_train.shape, X_train.shape, Y_val.shape, X_val.shape
+
+# LightGBM ile Zaman Serisi Modeli
+
+lgb_params = {'num_leaves': 10,
+              'learning_rate': 0.02,
+              'feature_fraction': 0.8,
+              'max_depth': 5,
+              'verbose': 0,
+              'num_boost_round': 10000,
+              'early_stopping_rounds': 200,
+              'nthread': -1}
+
+lgbtrain = lgb.Dataset(data=X_train, label=Y_train, feature_name=cols)
+
+lgbval = lgb.Dataset(data=X_val, label=Y_val, reference=lgbtrain, feature_name=cols)
+
+model = lgb.train(lgb_params, lgbtrain,
+                  valid_sets=[lgbtrain, lgbval],
+                  num_boost_round=lgb_params['num_boost_round'],
+                  early_stopping_rounds=lgb_params['early_stopping_rounds'],
+                  feval=lgbm_smape,
+                  verbose_eval=100)
+"""
+[9700]	training's SMAPE: 12.7678	valid_1's SMAPE: 12.1472
+[9800]	training's SMAPE: 12.765	valid_1's SMAPE: 12.1468
+[9900]	training's SMAPE: 12.762	valid_1's SMAPE: 12.1465
+[10000]	training's SMAPE: 12.7591	valid_1's SMAPE: 12.1458
+Did not meet early stopping. Best iteration is:
+[10000]	training's SMAPE: 12.7591	valid_1's SMAPE: 12.1458
+
+"""
+y_pred_val = model.predict(X_val, num_iteration=model.best_iteration)
+
+smape(np.expm1(y_pred_val), np.expm1(Y_val))
+
+
+# Değişken Önem Düzeyleri
+
+def plot_lgb_importances(model, plot=False, num=10):
+    gain = model.feature_importance('gain')
+    feat_imp = pd.DataFrame({'feature': model.feature_name(),
+                             'split': model.feature_importance('split'),
+                             'gain': 100 * gain / gain.sum()}).sort_values('gain', ascending=False)
+    if plot:
+        plt.figure(figsize=(10, 10))
+        sns.set(font_scale=1)
+        sns.barplot(x="gain", y="feature", data=feat_imp[0:25])
+        plt.title('feature')
+        plt.tight_layout()
+        plt.show()
+    else:
+        print(feat_imp.head(num))
+    return feat_imp
+
+
+plot_lgb_importances(model, num=30, plot=True)
+
+feat_imp = plot_lgb_importances(model, num=200)
+
+importance_zero = feat_imp[feat_imp["gain"] == 0]["feature"].values
+
+imp_feats = [col for col in cols if col not in importance_zero]
+len(imp_feats)
+
+# Final Model
+
+train = df.loc[~df.sales.isna()]
+Y_train = train['sales']
+X_train = train[cols]
+
+test = df.loc[df.sales.isna()]
+X_test = test[cols]
+
+lgb_params = {'num_leaves': 10,
+              'learning_rate': 0.02,
+              'feature_fraction': 0.8,
+              'max_depth': 5,
+              'verbose': 0,
+              'nthread': -1,
+              "num_boost_round": model.best_iteration}
+
+lgbtrain_all = lgb.Dataset(data=X_train, label=Y_train, feature_name=cols)
+
+final_model = lgb.train(lgb_params, lgbtrain_all, num_boost_round=model.best_iteration)
+
+test_preds = final_model.predict(X_test, num_iteration=model.best_iteration)
+
+# logaritma alınarak tekrardan gerçek değerlere döndürme
+np.expm1(test_preds)
+
+# Submission File
+test.head()
+
+submission_df = test.loc[:, ["id", "sales"]]
+submission_df['sales'] = np.expm1(test_preds)
+
+submission_df['id'] = submission_df.id.astype(int)
+
+submission_df.to_csv("submission_demand.csv", index=False)
